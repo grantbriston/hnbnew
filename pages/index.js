16c913316ad81b00b3a072782ad5106f823fd38e
Nathaniel Roe <nathanielroe@Nathaniels-MacBook-Pro-8.local>
2023-04-17 11:37:57 +0100

    feat: add header

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,8 @@ export default function Home() {
       <Hero />
       <SectionTitle
         pretitle="Nextly Benefits"
-        title=" Why should you use this landing page">
+        title=" Why should you use this landing page"
+      >
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
         completely open-source.
@@ -49,7 +50,8 @@ export default function Home() {
       <Benefits imgPos="right" data={benefitTwo} />
       <SectionTitle
         pretitle="Watch a video"
-        title="Learn how to fullfil your needs">
+        title="Learn how to fullfil your needs"
+      >
         This section is to highlight a promo or demo video of your product.
         Analysts says a landing page with video has 3% more conversion rate. So,
         don't forget to add one. Just like this.
@@ -57,7 +59,8 @@ export default function Home() {
       <Video />
       <SectionTitle
         pretitle="Testimonials"
-        title="Here's what our customers said">
+        title="Here's what our customers said"
+      >
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
       </SectionTitle>
